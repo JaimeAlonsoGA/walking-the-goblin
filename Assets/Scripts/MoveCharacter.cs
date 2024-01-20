53db1a2814155fe72c3cc9b978e44fda8c3937bc
kaoser <kaosermail@gmail.com>
2024-01-20 14:27:19 +0100

    add player movement

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isIdle = true;
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * Time.deltaTime * 10;
+            isIdle = false;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * Time.deltaTime * 10;
+            isIdle = false;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 300);
+            isIdle = false;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 300);
+            isIdle = false;
+        }
+
+        if(isIdle)
+        {
+            GetComponent<Animator>().SetBool("isIdle", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isIdle", false);
+        }
+    }
+}
