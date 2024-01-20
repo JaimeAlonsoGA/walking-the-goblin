bd063267013c6a4302140407bf2fc74e3e368f91
Jaime <alonsog.jaime@gmail.com>
2024-01-20 15:55:02 +0100

    add run keycode

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -13,36 +13,41 @@ public class MoveCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool isIdle = true;
+        int state = 0;
 
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += transform.forward * Time.deltaTime * 10;
-            isIdle = false;
+            transform.position += transform.forward * Time.deltaTime * 20;
+            state = 1;
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += transform.forward * Time.deltaTime * -10;
-            isIdle = false;
+            transform.position += transform.forward * Time.deltaTime * -20;
+            state = 1;
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 300);
-            isIdle = false;
+            state = 1;
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 300);
-            isIdle = false;
+            state = 1;
         }
 
-        if(isIdle)
+        if(state == 1)
         {
-            GetComponent<Animator>().SetBool("isIdle", true);
+            GetComponent<Animator>().SetInteger("state", 1);
         }
         else
         {
-            GetComponent<Animator>().SetBool("isIdle", false);
+            GetComponent<Animator>().SetInteger("state", 0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && state == 1)
+        {
+            GetComponent<Animator>().SetInteger("state", 2);
         }
     }
 }
