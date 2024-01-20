dffd2dfc12ee9b1197094f43fde3ecca0fbd20dd
Jaime <alonsog.jaime@gmail.com>
2024-01-20 18:18:43 +0100

    add mushrooms and mushroom spawn

---
+++ Assets/GoblinMushroomInteraction.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GoblinMushroomInteraction : MonoBehaviour
+{
+    public int mushroomsEated = 0;
+    public Mushroom[] mushrooms = new Mushroom[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Mushroom")
+        {
+            Mushroom mushroom = mushrooms.ToList().Find((x) => x == other.gameObject.GetComponent<Mushroom>());
+            mushroom.SpanwRandomPosition();
+            mushroomsEated++;
+            print(mushroom.transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
