1f6fa82080418f29a1a80fb0e70416ed89cccbd8
kaoser <kaosermail@gmail.com>
2024-01-21 13:16:18 +0100

    increase stamina bar with mushrooms, change color when low stamina

--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -11,15 +11,24 @@ public class Stamina : MonoBehaviour
     public float stamina = 100;
     float resetTimer = 0;
 
+    Image fill;
+    RectTransform background;
+    RectTransform fillArea;
+
     // Start is called before the first frame update
     void Start()
     {
+        fill = GetComponentsInChildren<Image>()[1];
+        background = GetComponentsInChildren<RectTransform>()[1];
+        fillArea = GetComponentsInChildren<RectTransform>()[2];
     }
 
     // Update is called once per frame
     void Update()
     {
         maxStamina = 100 + goblinMushroomInteraction.mushroomsEated * 5;
+        background.offsetMax = new Vector2(goblinMushroomInteraction.mushroomsEated * 5, background.offsetMax.y);
+        fillArea.offsetMax = new Vector2(goblinMushroomInteraction.mushroomsEated * 5 - 6.08f, fillArea.offsetMax.y);
 
         if (Input.GetKey(KeyCode.LeftShift) && stamina >= 1)
         {
@@ -38,6 +47,8 @@ public class Stamina : MonoBehaviour
         {
             stamina = Mathf.Clamp(stamina + Time.deltaTime * 10, 0, maxStamina);
         }
+
+        fill.color = Color.Lerp(Color.red, Color.green, stamina / maxStamina);
 
         GetComponent<Slider>().value = stamina / maxStamina;
     }
