ffb4a9b09c1153eed410434461dbf8a95b346d0f
kaoser <kaosermail@gmail.com>
2024-01-21 16:32:36 +0100

    add mountains, change movement for collision with them

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -17,13 +17,15 @@ public class MoveCharacter : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += transform.forward * Time.deltaTime * velocidad;
+            // transform.position += transform.forward * Time.deltaTime * velocidad;
+            GetComponent<Rigidbody>().velocity = transform.forward * Time.deltaTime * velocidad;
             state = stateParam;
         }
 
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += transform.forward * Time.deltaTime * -velocidad;
+            // transform.position += transform.forward * Time.deltaTime * -velocidad;
+            GetComponent<Rigidbody>().velocity = transform.forward * Time.deltaTime * -velocidad;
             state = stateParam;
         }
     }
@@ -46,14 +48,14 @@ public class MoveCharacter : MonoBehaviour
     void Update()
     {
         state = 0;
-        SpeedState(10, 1);
+        SpeedState(100, 1);
         RotationState();
 
         if (state == 1)
         {
             if (Input.GetKey(KeyCode.LeftShift) && stamina.stamina > 1)
             {
-                SpeedState(10, 2);
+                SpeedState(250, 2);
                 GetComponent<Animator>().SetInteger("state", 2);
             }
             else
@@ -64,6 +66,7 @@ public class MoveCharacter : MonoBehaviour
         else
         {
             GetComponent<Animator>().SetInteger("state", 0);
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 }
