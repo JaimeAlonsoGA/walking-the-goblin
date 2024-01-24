90ff63d07f393922846d44f3b7d7f3eeee90a6e2
Jaime <alonsog.jaime@gmail.com>
2024-01-24 19:14:47 +0100

    change the sound to be triggered by the animation

--- Assets/Scripts/Audio/EnemySound.cs
+++ Assets/Scripts/Audio/EnemySound.cs
@@ -9,26 +9,29 @@ using UnityEngine;
 public class EnemySound : MonoBehaviour
 {
     private FMOD.Studio.EventInstance tensionStrings;
-    private FMOD.Studio.EventInstance trollFootsteps;
+    // private FMOD.Studio.EventInstance trollFootsteps;
     public GoblinEnemyInteraction goblinEnemyInteraction;
-
+    void PlaySound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
     void Start()
     {
         tensionStrings = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Tension SFX");
-        trollFootsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Troll/footsteps");
+        // trollFootsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Troll/footsteps");
 
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(tensionStrings, GetComponent<Transform>(), GetComponent<Rigidbody>());
-        FMODUnity.RuntimeManager.AttachInstanceToGameObject(trollFootsteps, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        // FMODUnity.RuntimeManager.AttachInstanceToGameObject(trollFootsteps, GetComponent<Transform>(), GetComponent<Rigidbody>());
 
-        trollFootsteps.start();
+        // trollFootsteps.start();
         tensionStrings.start();
 
-        PLAYBACK_STATE playbackState;
-        trollFootsteps.getPlaybackState(out playbackState);
-        if (playbackState.Equals(PLAYBACK_STATE.PLAYING))
-        {
-            print("troll footsteps playing");
-        }
+        // PLAYBACK_STATE playbackState;
+        // trollFootsteps.getPlaybackState(out playbackState);
+        // if (playbackState.Equals(PLAYBACK_STATE.PLAYING))
+        // {
+        //     print("troll footsteps playing");
+        // }
 
     }
 
@@ -38,7 +41,7 @@ public class EnemySound : MonoBehaviour
         if (goblinEnemyInteraction.isGameOver == true)
         {
             tensionStrings.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
-            trollFootsteps.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            // trollFootsteps.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         }
     }
 }
--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -10,8 +10,8 @@ using UnityEngine.Rendering;
 public class SoundManager : MonoBehaviour
 {
     // public static SoundManager Instance {get; private set; }
-    private FMOD.Studio.EventInstance walkInstance;
-    private FMOD.Studio.EventInstance runInstance;
+    // private FMOD.Studio.EventInstance walkInstance;
+    // private FMOD.Studio.EventInstance runInstance;
     private FMOD.Studio.EventInstance goblinGroanInstance;
     private FMOD.Studio.EventInstance ambienceInstance;
 
@@ -19,18 +19,23 @@ public class SoundManager : MonoBehaviour
 
     void Start()
     {   
-        walkInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Goblin/Goblin Footsteps Walk");
-        runInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Goblin/Goblin Footsteps Run");
+        // walkInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Goblin/Goblin Footsteps Walk");
+        // runInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Goblin/Goblin Footsteps Run");
         goblinGroanInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Goblin/Goblin Groan");
         ambienceInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Ambience");
 
-        FMODUnity.RuntimeManager.AttachInstanceToGameObject(walkInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
-        FMODUnity.RuntimeManager.AttachInstanceToGameObject(runInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        // FMODUnity.RuntimeManager.AttachInstanceToGameObject(walkInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        // FMODUnity.RuntimeManager.AttachInstanceToGameObject(runInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(goblinGroanInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(ambienceInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
         
         ambienceInstance.start();
         goblinGroanInstance.start();
+    }
+
+    void PlaySound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
     }
 
     void Update()
@@ -41,33 +46,33 @@ public class SoundManager : MonoBehaviour
             ambienceInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         }
 
-        if (GetComponent<Animator>().GetInteger("state") == 2)
-        {
-            PLAYBACK_STATE playbackState;
-            runInstance.getPlaybackState(out playbackState);
-            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
-            {
-                runInstance.start();
-            }
-        }
-        else
-        {
-            runInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        }
+        // if (GetComponent<Animator>().GetInteger("state") == 2)
+        // {
+        //     PLAYBACK_STATE playbackState;
+        //     runInstance.getPlaybackState(out playbackState);
+        //     if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+        //     {
+        //         // runInstance.start();
+        //     }
+        // }
+        // else
+        // {
+        //     // runInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        // }
 
-        if (GetComponent<Animator>().GetInteger("state") == 1)
-        {
-            PLAYBACK_STATE playbackState;
-            walkInstance.getPlaybackState(out playbackState);
-            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
-            {
-                walkInstance.start();
-            }
-        }
-        else
-        {
-            walkInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        }
+        // if (GetComponent<Animator>().GetInteger("state") == 1)
+        // {
+        //     PLAYBACK_STATE playbackState;
+        //     walkInstance.getPlaybackState(out playbackState);
+        //     if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+        //     {
+        //         // walkInstance.start();
+        //     }
+        // }
+        // else
+        // {
+        //     // walkInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        // }
 
     }
 
